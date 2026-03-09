fe5d17a029ce7e3b5ecd94e6aa0a4b3f12db9686
Jukka Suomela <jukka.suomela@iki.fi>
2026-03-09 12:55:49 +0200

    A file for easily checking what axioms were used

---
+++ Distributed2Coloring/AxiomAudit.lean
@@ -0,0 +1,5 @@
+import Distributed2Coloring.API
+
+#print axioms Distributed2Coloring.pStar_ge_23879
+#print axioms Distributed2Coloring.pStar_le_24118
+#print axioms Distributed2Coloring.pStar_lt_24118
